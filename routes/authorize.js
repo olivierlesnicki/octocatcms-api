97872b83d94473fd42f5a91b43731ef074c3d2aa
Olivier Lesnicki <olivier.lesnicki@gmail.com>
2016-07-08 22:57:55 +0100

    Removed env.json

--- routes/authorize.js
+++ routes/authorize.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var request = require('request-promise');
-var env = require('var');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
   request.post('https://github.com/login/oauth/access_token', {
     body: {
-      client_id: env.GITHUB_CLIENT_ID,
-      client_secret: env.GITHUB_CLIENT_SECRET,
+      client_id: process.env.GITHUB_CLIENT_ID,
+      client_secret: process.env.GITHUB_CLIENT_SECRET,
       code: req.query.code
     },
     json: true
