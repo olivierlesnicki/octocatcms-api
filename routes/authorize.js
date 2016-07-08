83e299a75db7fbe0ec3e57c5959cb394c8537954
Olivier Lesnicki <olivier.lesnicki@gmail.com>
2016-07-08 18:04:55 +0100

    Fixed code

--- routes/authorize.js
+++ routes/authorize.js
@@ -4,16 +4,21 @@ var env = require('var');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  request({
-    uri: 'https://github.com/login/oauth/access_token',
-    method: 'POST',
+  request.post('https://github.com/login/oauth/access_token', {
     body: {
       client_id: env.GITHUB_CLIENT_ID,
       client_secret: env.GITHUB_CLIENT_SECRET,
       code: req.query.code
-    }
+    },
+    json: true
   }).then(body => {
-    res.redirect(req.query.redirect_uri);
+    if (!body.error) {
+      res.redirect('/?access_token=');
+    } else {
+      next(body.error);
+    }
+  }, response => {
+    next(response.error);
   });
 });
 
